1cc59b7bd312cf369fe5729c6afe16c9f7431ec6
Angela <105812585+higuruma@users.noreply.github.com>
2023-10-21 18:13:05 -0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+  print "JJJSs";
+  ECHO "bqayb<br>";
+EcHo "echo!<br>";
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>my first PHP</title>
+  </head>
+  <body>
+
+      <h2><?php echo 'a heading 2'; ?></h2>
+      <h1></h1>
+
+      <script src="scripts/main.js"></script>
+  </body>
+</html>
