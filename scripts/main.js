827782856d87d6806fed4af658be6ae76287ec08
higuruma <angelahan4046@gmail.com>
2023-10-24 14:57:52 -0700

    const heading h2

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 var msg = 'Hello  World';
 console.log(msg);
 const myHeading = document.querySelector("h1");
+const heading = document.querySelector("h2");
 myHeading.textContent = "Helwerlo world!";
