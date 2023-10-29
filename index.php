6912e563f0e6c32e301c8511de471d2f3b1f1af9
extraclowny <lemonade584521@gmail.com>
2023-10-28 23:46:45 -0700

    changes to db

--- index.php
+++ index.php
@@ -1,17 +1,54 @@
 
 <?php
-  echo "hell";
+  echo "Database Initation!";
+
+  //variables
+
+  $hostURL = "localhost";
+  $db_username = "fullbug";
+  $db_password = "honeytest";
+  $db_name = "shopaby";
+
+  // making new DB connection
+
+  $conn = mysqli_connect($hostURL, $db_username, $db_password, $db_name);
+  if(!$conn){
+    echo 'Connection error: ' , mysqli_connect_error();
+  }
+
+  // write query for all users
+  // SELECT = RETRIEVE, FROM = GET FROM
+  // star (*) means u want all the columns for each record
+  // if no want all, type them out instead
+
+  $sql = 'SELECT first_name, last_name FROM users';
+
+  // make query and get result
+  // uses $conn variable ref to connect
+
+  $result = mysqli_query($conn, $sql);
+
+  // Have to get from result the array that want
+  // fetch resulting rows
+  // returns $result as associative array
+
+  $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  // printing array
+  echo "</br>". "Fetched Users". "</br>";
+
+  print_r($users);
+
 ?>
 
 <!DOCTYPE html>
 <html>
   <head>
-    <title>SHOPABY - HOME</title>
-    <link rel="stylesheet" href="styles.css">
-  </head>
+    <title>my first PHP</title>
+      </head>
   <body>
 
-      <h1>hello</h1>
+      <h1><?php echo "GEESH"; ?></h1>
 
       <script src="scripts/main.js"></script>
   </body>
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,2 @@
 var msg = 'Hello  World';
 console.log(msg);
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Helwerlo world!";
