a80ad4336672be6a21443786a1c77a3c5a9b7ab8
VaporificFawn55 <jayxu231@gmail.com>
2023-11-11 13:38:06 -0800

    uploading and displaying page

---
+++ displaytest.php
@@ -0,0 +1,21 @@
+<?php
+    include 'db_connect.php'
+    // connecting to db
+    $pdo = new PDO('mysql:host=localhost;dbname=shopaby', $db_username, $db_password);
+    // getting image form db
+    $stmt = $pdo->prepare("SELECT naem, type data FROM images WHERE id=?");
+    $stmt->bindParam(1, $id);
+    $stmt->execure();
+
+    header("Content-Type: image/jpeg");
+    // output image
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo $row['data'];
+?>
+
+<DOCTYPE html>
+    <html>
+    <img src="display.php?id=1" />
+
+
+    </html>
